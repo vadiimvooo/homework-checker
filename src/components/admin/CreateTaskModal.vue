396cf12f41c78597c7b66a7bcafde32db47f941a
Vadym <asd4s.profile@gmail.com>
2023-11-10 18:16:40 +0200

    Added basic logic with dialog window

---
+++ src/components/admin/CreateTaskModal.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+  const { visible } = defineProps({
+    visible: {
+      type: Boolean,
+      required: true
+    }
+  })
+
+  const emits = defineEmits(['closeModal'])
+
+  const closeModal = () => {
+    console.log(11231)
+    emits('closeModal');
+  }
+</script>
+
+<template>
+  <Dialog
+      :visible="visible"
+      modal
+      header="Header"
+      :style="{ width: '50rem' }"
+      :breakpoints="{ '1199px': '75vw', '575px': '90vw' }"
+      :close-on-escape="true"
+      @keyup.enter="closeModal"
+  >
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+      Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+    </p>
+  </Dialog>
+</template>
+
+<style scoped>
+</style>
--- src/components/admin/Tasks.vue
+++ src/components/admin/Tasks.vue
@@ -1,8 +1,35 @@
 <script setup lang="ts">
+  import CreateTaskModal from "@/components/admin/CreateTaskModal.vue";
+  import {onMounted, onUpdated, ref} from "vue";
+  const isCreateModalOpened = ref(false);
+
+  onMounted(() => {
+    console.log(isCreateModalOpened.value);
+  })
+
+  onUpdated(() => {
+    console.log(isCreateModalOpened.value);
+  })
+  const onTaskCreation = () => {
+    isCreateModalOpened.value = true;
+  }
+
+  const onCLoseModal = () => {
+    isCreateModalOpened.value = false;
+  }
 </script>
 
 <template>
-  Tasks
+  <Button
+      icon="pi pi-plus"
+      severity="info"
+      rounded
+      aria-label="Search"
+      class="w-[45px] h-[45px] absolute bottom-10 right-10"
+      @click="onTaskCreation()"
+      @close-modal="onCLoseModal"
+  />
+  <CreateTaskModal :visible="isCreateModalOpened" />
 </template>
 
 <style scoped>
